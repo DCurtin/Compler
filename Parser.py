61deef4089ef6673d5b9a2c4843ca75668e700fb
Dalton Curtin <DaltonCurtin@gmail.com>
2018-02-23 00:51:44 -0500

    Progress on parser, going to simply by move code to functions

--- Parser.py
+++ Parser.py
@@ -12,53 +12,77 @@ class Parser(object):
             [1] = first_arg
             [2] = second_arg (some operators are unary)
         '''
-        bracketStack = []
+        bracketCnt = 0
         linePose = 0
         if line[linePose] == "(": # open bracket found, get operator, primitive, or argument
             lineItems = ["","",""]
-            bracketStack.append(line[linePose])
+            bracketCnt += 1
             linePose += 1
             
             while line[linePose] != ")" and line[linePose] != "(" and line[linePose] != " ": # step through line until the end of the operator, primitive, or argument is found
                 lineItems[0] += line[linePose]
                 linePose += 1
                 
+            while line[linePose] == " ":
+                linePose += 1 # skip white space
+                
             if line[linePose] == ")": # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
-                bracketStack.pop()
-                if len(bracketStack) > 0:
+                bracketCnt -= 1
+                if bracketCnt is not 0:
                     #bracket miscount throw exception
                     print("bracket miscount exception")
                 else:
                     return lineItems
             
+            if line[linePose] == "(": # if mid arg has open braces
+                firstArgLst = self.parseBracketVar(line, linePose)
+                lineItems[1] = firstArgLst[0]
+                linePose = firstArgLst[1]
+                
+            if lineItems[1] == "":
+                while line[linePose] != ")" and line[linePose] != "(" and line[linePose] != " ": # step through line until the end of the operator, primitive, or argument is found
+                lineItems[1] += line[linePose]
+                linePose += 1
+                
             while line[linePose] == " ":
                 linePose += 1 # skip white space
-            
-            if line[linePose] == "(": # if mid arg has open braces
-            
+                
+                
+            if line[linePose] == ")": # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
+            bracketCnt -= 1
+            if bracketCnt is not 0:
+                #bracket miscount throw exception
+                print("bracket miscount exception")
+            else:
+                return lineItems
+        
             ''' 
                 if line[linePose] == "(": # start of first argument
-                bracketStack.append(line[linePose])
+                bracketCnt.append(line[linePose])
                 while line[linePose] != ")":
             '''
                     
                 
     def parseBracketVar(self, line, linePose): #travels a set of parentheses e.g. ((( something ))) collecting and keeping count of open and closed parentheses returns new linePose and str of parentheses
         
-                    bracketStack.push(line[linePose])
-                    lineItems[1] += line[linePose]
+                    bracketCnt = 1
+                    lineItem = line[linePose]
                     linePose += 1
                     
-                    while len(bracketStack) > 0 and linePose < len(line): # run through middle arg up to the last closing bracket
+                    '''
+                        Capture from left most bracket to right most
+                    '''
+                    while bracketCnt > 0 and linePose < len(line): 
                     
                         if line[linePose] == "(":
-                            bracketStack.push("(")
+                            bracketCnt += 1
                             
                         if line[linePose] == ")":
-                            bracketStack.pop()
+                            bracketCnt -= 1
                             
-                        lineItems[1] += line[linePose]
+                        lineItem += line[linePose]
                         linePose += 1
-                    # traversed entire argument
+                    
+                    return lineItem, linePose
             
         
