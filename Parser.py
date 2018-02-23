8bceef54c68efc65744bb3f68377fab7fe909c6e
Dalton Curtin <DaltonCurtin@gmail.com>
2018-02-23 02:42:39 -0500

    Changed so returned list is only as big as pulled args

--- Parser.py
+++ Parser.py
@@ -1,107 +1,107 @@
 '''
     Class for handling parsing
 '''
-class Parser(object):
-    # FRONT_BRACKETS = ["[","("]
-    # END_BRACKETS = ["]",")"]
-    
-    def resolveLayer(self, line):
-        '''
-            Is given a line as input and returns a list of
-            [0] = val, variable, or operator
-            [1] = first_arg
-            [2] = second_arg (some operators are unary)
-        '''
-        bracketCnt = 0
-        linePose = 0
-        if line[linePose] == "(": # open bracket found, get operator, primitive, or argument
-            lineItems = ["","",""]
-            bracketCnt += 1
-            linePose += 1
-
-            while line[linePose] != ")" and line[linePose] != "(" and line[linePose] != " ": # step through line until the end of the operator, primitive, or argument is found
-                lineItems[0] += line[linePose]
-                linePose += 1
-
-            linePose = self.skipWhiteSpace(line, linePose)
-
-            if self.checkEnd(line, linePose, bracketCnt): # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
-                return lineItems
-
-            
-            
-            lineItems[1], linePose = self.parseArg(line, linePose)
-            
-
-            linePose = self.skipWhiteSpace(line, linePose)
-            
-            if self.checkEnd(line, linePose, bracketCnt): # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
-                return lineItems
-            
-            
-            
-            lineItems[2], linePose = self.parseArg(line, linePose)
-            
-            linePose = self.skipWhiteSpace(line, linePose)
-            
-            if self.checkEnd(line, linePose, bracketCnt): # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
-                return lineItems
-            else:
-                print("Failed to Parse")
-                return 0
 
     
-    def skipWhiteSpace(self, line, linePose): #returns linePose after white space
-        while line[linePose] == " ":
-                linePose += 1 # skip white space
-        return linePose
-        
-    def checkEnd(self, line, linePose, bracketCnt):
-        if line[linePose] == ")": # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
-                bracketCnt -= 1
-                if bracketCnt is not 0:
-                    #bracket miscount throw exception
-                    print("bracket miscount exception")
-                else:
-                    return True
-        else:
-            return False
-
-    def parseBracketVar(self, line, linePose): 
-        '''
-            travels a set of parentheses e.g. ((( something ))) collecting and 
-            keeping count of open and closed parentheses returns new linePose 
-            and str of parentheses
-        '''
-        bracketCnt = 1
-        lineItem = line[linePose]
+def resolveLayer(line):
+    '''
+        Is given a line as input and returns a list of
+        [0] = val, variable, or operator
+        [1] = first_arg
+        [2] = second_arg (some operators are unary)
+    '''
+    bracketCnt = 0
+    linePose = 0
+    if line[linePose] == "(": # open bracket found, get operator, primitive, or argument
+        lineItems = []
+        bracketCnt += 1
         linePose += 1
 
-        '''
-            Capture from left most bracket to right most
-        '''
-        while bracketCnt > 0 and linePose < len(line): 
-
-            if line[linePose] == "(":
-                bracketCnt += 1
-
-            if line[linePose] == ")":
-                bracketCnt -= 1
-
-            lineItem += line[linePose]
+        while line[linePose] != ")" and line[linePose] != "(" and line[linePose] != " ": # step through line until the end of the operator, primitive, or argument is found
+            lineItems.append(line[linePose])
             linePose += 1
 
-        return lineItem, linePose
+        linePose = skipWhiteSpace(line, linePose)
+
+        if checkEnd(line, linePose, bracketCnt): # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
+            return lineItems
 
 
-    def parseArg(self, line, linePose):
-        if line[linePose] == "(": # if mid arg has open braces
-            return self.parseBracketVar(line, linePose)
+        itemList = parseArg(line, linePose)
+        lineItems.append(itemList[0])
+        linePose = itemList[1]
 
-        else:
-            lineItem = ""
-            while line[linePose] != ")" and line[linePose] != "(" and line[linePose] != " ": # step through line until the end of the operator, primitive, or argument is found
-                lineItem += line[linePose]
-                linePose += 1
-            return lineItem, linePose
+
+        linePose = skipWhiteSpace(line, linePose)
         
+        if checkEnd(line, linePose, bracketCnt): # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
+            return lineItems
+
+
+        itemList = parseArg(line, linePose)
+        lineItems.append(itemList[0])
+        linePose = itemList[1]
+        
+        linePose = skipWhiteSpace(line, linePose)
+        
+        if checkEnd(line, linePose, bracketCnt): # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
+            return lineItems
+        else:
+            print("Failed to Parse")
+            return 0
+
+
+def skipWhiteSpace(line, linePose): #returns linePose after white space
+    while line[linePose] == " ":
+            linePose += 1 # skip white space
+    return linePose
+    
+def checkEnd(line, linePose, bracketCnt):
+    if line[linePose] == ")": # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
+            bracketCnt -= 1
+            if bracketCnt is not 0:
+                #bracket miscount throw exception
+                print("bracket miscount exception")
+            else:
+                return True
+    else:
+        return False
+
+def parseBracketVar(line, linePose): 
+    '''
+        travels a set of parentheses e.g. ((( something ))) collecting and 
+        keeping count of open and closed parentheses returns new linePose 
+        and str of parentheses
+    '''
+    bracketCnt = 1
+    lineItem = line[linePose]
+    linePose += 1
+
+    '''
+        Capture from left most bracket to right most
+    '''
+    while bracketCnt > 0 and linePose < len(line): 
+
+        if line[linePose] == "(":
+            bracketCnt += 1
+
+        if line[linePose] == ")":
+            bracketCnt -= 1
+
+        lineItem += line[linePose]
+        linePose += 1
+
+    return lineItem, linePose
+
+
+def parseArg(line, linePose):
+    if line[linePose] == "(": # if mid arg has open braces
+        return parseBracketVar(line, linePose)
+
+    else:
+        lineItem = ""
+        while line[linePose] != ")" and line[linePose] != "(" and line[linePose] != " ": # step through line until the end of the operator, primitive, or argument is found
+            lineItem += line[linePose]
+            linePose += 1
+        return lineItem, linePose
+    
