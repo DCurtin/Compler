2373a65e643f2a6be5f18ce998de13004790cf09
Dalton Curtin <DaltonCurtin@gmail.com>
2018-02-25 20:50:48 -0500

    Modified Parser to handle brackets and lets differently. Need to add proper exception handling and test out more

--- Parser.py
+++ Parser.py
@@ -10,23 +10,29 @@ class Parser:
             [1] = first_arg
             [2] = second_arg (some operators are unary)
         '''
-        bracketCnt = 0
+        bracketStck = []
         linePose = 0
         if line[linePose] == "(": # open bracket found, get operator, primitive, or argument
             lineItems = []
-            bracketCnt += 1
-            linePose += 1
             operand = ""
-            while line[linePose] != ")" and line[linePose] != "(" and line[linePose] != " ": # step through line until the end of the operator, primitive, or argument is found
+
+            while line[linePose] == "(" or line[linePose] == "[":
+                bracketStck.append(line[linePose])
+                linePose += 1
+            
+            while line[linePose] != ")" and line[linePose] != "]" and line[linePose] != "(" and line[linePose] != " ": # step through line until the end of the operator, primitive, or argument is found
                 operand += line[linePose]
                 linePose += 1
             lineItems.append(operand)
     
-            while(not Parser.checkEnd(line, linePose, bracketCnt)):
+            while(not Parser.checkEnd(line, linePose, bracketStck)):
                 linePose = Parser.skipWhiteSpace(line, linePose)
                 itemList = Parser.parseArg(line, linePose)
                 lineItems.append(itemList[0])
                 linePose = itemList[1]
+                
+            # if linePose < len(line):
+            #     print("closed all brackets before line end")
                 
             
             return lineItems
@@ -36,14 +42,26 @@ class Parser:
                 linePose += 1 # skip white space
         return linePose
         
-    def checkEnd(line, linePose, bracketCnt):
-        if line[linePose] == ")": # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
-                bracketCnt -= 1
-                if bracketCnt is not 0:
-                    #bracket miscount throw exception
-                    print("bracket miscount exception")
-                else:
-                    return True
+    def checkEnd(line, linePose, bracketStck):
+        print(bracketStck)
+        if linePose >= len(line) and len(bracketStck) != 0:
+            print("End of line before closed brackets")
+            return True
+        while line[linePose] == ")" or line[linePose] == "]": # end of line, check for miscount on brackets otherwise lineItem is a primitive or variable
+                bracket = bracketStck.pop()
+                
+                if line[linePose] == "]" and bracket == "(":
+                    print("%s, %s" % (bracket, line[linePose]))
+                    print("Bracket close mismatch")
+
+                if line[linePose] == ")" and bracket == "[":
+                    print("%s, %s" % (bracket, line[linePose]))
+                    print("Bracket close mismatch")
+
+                linePose += 1
+                if linePose >= len(line):
+                        return True
+
         else:
             return False
     
@@ -53,20 +71,29 @@ class Parser:
             keeping count of open and closed parentheses returns new linePose 
             and str of parentheses
         '''
-        bracketCnt = 1
         lineItem = line[linePose]
+        bracketStck = [lineItem]
         linePose += 1
     
         '''
             Capture from left most bracket to right most
         '''
-        while bracketCnt > 0 and linePose < len(line): 
+        while len(bracketStck) > 0 and linePose < len(line): 
     
             if line[linePose] == "(":
-                bracketCnt += 1
+                bracketStck.append("(")
+                
+            if line[linePose] == "[":
+                bracketStck.append("[")
     
             if line[linePose] == ")":
-                bracketCnt -= 1
+                if bracketStck.pop() != "(":
+                    print("Closing wrong bracket type")
+                    
+            if line[linePose] == "]":
+                if bracketStck.pop() != "[":
+                    print("Closing wrong bracket type")
+                
     
             lineItem += line[linePose]
             linePose += 1
@@ -80,7 +107,7 @@ class Parser:
     
         else:
             lineItem = ""
-            while line[linePose] != ")" and line[linePose] != "(" and line[linePose] != " ": # step through line until the end of the operator, primitive, or argument is found
+            while line[linePose] != "[" and line[linePose] != "]" and line[linePose] != ")" and line[linePose] != "(" and line[linePose] != " ": # step through line until the end of the operator, primitive, or argument is found
                 lineItem += line[linePose]
                 linePose += 1
             return lineItem, linePose
