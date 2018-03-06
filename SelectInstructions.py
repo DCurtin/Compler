03c7fe426dfba8bd332aa4b565872da9c2aae555
Dalton Curtin <DaltonCurtin@gmail.com>
2018-03-05 21:33:29 -0500

    Added (int someArg) to output, handled by resolveInt() function that returns an int wrapped in that opperand and braces.

--- SelectInstructions.py
+++ SelectInstructions.py
@@ -17,24 +17,24 @@ def selectInstructions(line):
             lineOp = Parser.resolveLayer(lineLst[2])
 
             if len(lineOp) < 2:
-                lineAsm += "(movq " + lineOp + " " + lineLst[1] + ") "
+                lineAsm += "(movq " + resolveInt(lineOp) + " " + lineLst[1] + ") "
             else:
                 if lineOp[0] == "+":
-                    lineAsm += "(movq " + lineOp[1] + " " + lineLst[1] + ") "
-                    lineAsm += "(addq " + lineOp[2] + " " + lineLst[1] + ")  "
+                    lineAsm += "(movq " + resolveInt(lineOp[1]) + " " + lineLst[1] + ") "
+                    lineAsm += "(addq " + resolveInt(lineOp[2]) + " " + lineLst[1] + ")  "
                 if lineOp[0] == "-":
-                    lineAsm += "(movq 0 " + lineLst[1] + ") "
-                    lineAsm += "(subq " + lineOp[1] + " " + lineLst[1] + ") "
+                    lineAsm += "(movq (int 0) " + lineLst[1] + ") "
+                    lineAsm += "(subq " + resolveInt(lineOp[1]) + " " + lineLst[1] + ") "
     asmAndVars = ["(program " + lineAsm.strip() + ")", program[1]]
     return asmAndVars
 
-def isDecimal(input):
+def resolveInt(input): # not crazy about this work-around though it's more robust than other methods of checking int that I am aware of 
     try:
         int(input)
-        return True
-    except error:
-        return False
+        return "(int " + input + ")"
+    except:
+        return input
 
-def resolveInt(input):
-    if isDecimal(input[1]):
-        isDecimal[0]
+# def resolveInt(input):
+#    if isDecimal(input[1]):
+#        isDecimal[0]
